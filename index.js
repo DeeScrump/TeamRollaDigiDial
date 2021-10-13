b224549b8af22cf02225d24fd8314777171f5a37
DCKII <dckornbluthii@bellsouth.net>
2021-10-12 20:38:29 -0400

    updated questions array

--- index.js
+++ index.js
@@ -3,36 +3,39 @@ const fs = require('fs');
 
 const generateHTML = (answers) =>
   `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
-  <title>Document</title>
-</head>
-<body>
-  <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">My Team!</h1>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+    <title>My Team Profile</title>
+  </head>
+  <body>
+    <div class="jumbotron jumbotron-fluid bg-primary">
+    <div class="container">
+      <h1 class="display-4 text-center">My Team!</h1>
+    </div>
   </div>
-</div>
-
-<div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-    <ul class="list-group list-group-flush">
-        <li class="list-group-item">An item</li>
-        <li class="list-group-item">A second item</li>
-        <li class="list-group-item">A third item</li>
-    </ul>
+  
+  <div class="card border" style="width: 18rem;">
+    <div class="card-body p-0">
+      <div class="card bg-danger">
+      <h5 class="card-title bg-danger">${answers.name}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">Manager</h6>
+      </div>
+      <ul class="list-group list-group-flush">
+          <li class="list-group-item border-0">${answers.id}</li>
+          <li class="list-group-item border-0">${answers.email}</li>
+          <li class="list-group-item border-0">${answers.office}</li>
+      </ul>
+    </div>
   </div>
-</div>
-</body>
-</html>
+  </body>
+  </html>
+  
 `;
 
-const generateQuestions = [
+const initialQuestions = [
     {
     type: 'input',
     name: 'name',
@@ -54,27 +57,92 @@ const generateQuestions = [
     message: `What is the team manager's office number?`,
     },
     {
-    type: 'checkbox',
+    type: 'list',
     name: 'newMember',
     choices: ['Engineer', 'Intern', 'I do not want to add any more team members.'],
     message: 'Which type of team member would you like to add?',
     },
+];
+
+const engineerQuestions = [
     {
     type: 'input',
-    name: 'linkedin',
-    message: 'Enter your LinkedIn URL.',
+    name: 'name',
+    message: `What is the team manager's name?`,
+    },
+    {
+    type: 'input',
+    name: 'id',
+    message: `What is the team manager's id?`,
+    },
+    {
+    type: 'input',
+    name: 'email',
+    message: `What is the team manager's email?`,
+    },
+    {
+    type: 'input',
+    name: 'github',
+    message: `What is the engineer's Github username?`,
+    },
+    {
+    type: 'list',
+    name: 'newMember',
+    choices: ['Engineer', 'Intern', 'I do not want to add any more team members.'],
+    message: 'Which type of team member would you like to add?',
+    },
+];
+
+const internQuestions = [
+    {
+    type: 'input',
+    name: 'name',
+    message: `What is the team manager's name?`,
+    },
+    {
+    type: 'input',
+    name: 'id',
+    message: `What is the team manager's id?`,
+    },
+    {
+    type: 'input',
+    name: 'email',
+    message: `What is the team manager's email?`,
+    },
+    {
+    type: 'input',
+    name: 'school',
+    message: `What is the intern's school?`,
+    },
+    {
+    type: 'list',
+    name: 'newMember',
+    choices: ['Engineer', 'Intern', 'I do not want to add any more team members.'],
+    message: 'Which type of team member would you like to add?',
     },
 ];
     
+// function endTeam (choice) {
+//     while(choice !== '')
+// }
+
+
+
 
 function init() {
 
     inquirer
-    .prompt(generateQuestions)
+    .prompt(initialQuestions)
     .then((answers) => {
-        fs.writeFile(fileName, READmeContent, (err) =>
-            err ? console.log(err) : console.log('Check out your README file!')
-        );  
+          console.log(answers)
+        //get the choice from answers
+        //call a function that will prompt the next set of questions
+
+        // const htmlPageContent = generateHTML(answers);
+
+        // fs.writeFile('dist/index.html', htmlPageContent, (err) =>
+        // err ? console.log(err) : console.log('Successfully created index.html!')
+        // );
     }); 
 }
 
